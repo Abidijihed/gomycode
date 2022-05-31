28a82abae7d55267f40bc411da099c14551a0c80
Abidijihed <abidij55@gmail.com>
2022-05-31 10:17:15 +0100

    first  test javascripte

---
+++ test.js
@@ -0,0 +1,22 @@
+//Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7.
+
+function areaTriangle(){
+    //code
+}
+// Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar. 
+function leapYear(){
+    //code
+}
+
+//  Write a JavaScript program to compute the sum of the two given integers. If the two values are same, then returns triple their sum
+function tripleSum(){
+    //code
+}
+// Write a JavaScript program to compute the absolute difference between a specified number and 19. Returns triple their absolute difference if the specified number is greater than 19
+function differenceNum(){
+    //code
+}
+// Write a JavaScript program to check two given numbers and return true if one of the number is 50 or if their sum is 50
+function check50(){
+    //code
+}
